66ddb2721c5327fd025b990a213826ebb27c67d2
Abhoy Sarkar <sarkar.ab07@gmail.com>
2024-03-05 22:13:23 +0530

    ui changes

--- app/(home)/_Components/Logo.tsx
+++ app/(home)/_Components/Logo.tsx
@@ -12,7 +12,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center gap-x-2">
       <a href="/" className="flex items-center">
-        <Image src="/logo.png" height="84" width="84" alt="Logo" />
+        <Image src="/assets/login.png" height="84" width="84" alt="Logo" />
       </a>
       <p
         className={cn("hidden md:flex font-semibold text-5xl", font.className)}
